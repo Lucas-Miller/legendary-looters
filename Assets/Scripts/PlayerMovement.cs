2d7989152bfd4564ea8f1a3590137e8a1168b198
Lucas Miller <millerlucas25@gmail.com>
2021-10-26 19:35:38 -0400

    Added basic player class with mouse look, WASD movement, space to jump, shift to run, health, armor, etc

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController controller;
+    public float health = 100f;
+    public float armor = 100f;
+    public float damage = 10f;
+
+
+    public GameObject currentWeapon;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    public float speed = 12.0f;
+    public float gravity = -9.81f * 2f;
+    public float jumpHeight = 1f;
+
+    Vector3 velocity;
+    bool isGrounded;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        if(Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            speed = speed * 2.0f;
+        } 
+        else if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            speed = 12.0f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
