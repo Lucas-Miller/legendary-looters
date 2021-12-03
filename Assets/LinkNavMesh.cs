992cb75a79d4c7957878591052ec3550bc1d8561
Lucas Miller <millerlucas25@gmail.com>
2021-12-03 16:44:45 -0500

    Added materials, torch, fixed an issue where enemies couldn't move from prefab to prefab, etc.

---
+++ Assets/LinkNavMesh.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.AI;
+using Unity.AI.Navigation;
+using UnityEngine;
+
+public class LinkNavMesh : MonoBehaviour
+{
+    private bool hasUpdated = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //hasUpdated = true;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var NavLinkData = GetComponent<NavMeshLink>();
+        NavLinkData.UpdateLink();
+        //var navLink = GetComponent<NavMeshLink>();
+    }
+}
